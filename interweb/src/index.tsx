3fd24b9050eea9d9b484df08187758c9bb525c6f
Matias <mmullerlanas@gmail.com>
2025-05-02 21:43:46 -0400

    refactor: replace lazy loading with direct imports for route components

--- interweb/src/index.tsx
+++ interweb/src/index.tsx
@@ -1,21 +1,21 @@
-import React, { Suspense, lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const Login = lazy(() => import('./home/login'));
-const Homeprocesos = lazy(() => import('./home/homeprocesos'));
-const Encol = lazy(() => import('./procesos/encol'));
-const Troquel = lazy(() => import('./procesos/troquel'));
-const Troza = lazy(() => import('./procesos/troz'));
-const Home = lazy(() => import('./home/home'));
-const EditEncolado = lazy(() => import('./procesos/edit/editencol'));
-const Edicion = lazy(() => import('./home/edicion'));
-const Emplacado = lazy(() => import('./procesos/emplac'));
-const ProcesosTable = lazy(() => import('./procesos/procesos'));
-const Mult = lazy(() => import('./procesos/mult'));
-const UserTable = lazy(() => import('./users/users'));
+import Login from './home/login';
+import Homeprocesos from './home/homeprocesos';
+import Encol from './procesos/encol';
+import Troquel from './procesos/troquel';
+import Troza from './procesos/troz';
+import Home from './home/home';
+import EditEncolado from './procesos/edit/editencol';
+import Edicion from './home/edicion';
+import Emplacado from './procesos/emplac';
+import ProcesosTable from './procesos/procesos';
+import Mult from './procesos/mult';
+import UserTable from './users/users';
 
 const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   const user = localStorage.getItem('user'); // Ensure this checks for 'user'
@@ -55,100 +55,98 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/home"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/programa-produccion"
-            element={
-              <ProtectedRoute>
-                <Homeprocesos />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edicion"
-            element={
-              <ProtectedRoute>
-                <Edicion />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/encolado"
-            element={
-              <ProtectedRoute>
-                <Encol />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/troquelado"
-            element={
-              <ProtectedRoute>
-                <Troquel />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/trozado"
-            element={
-              <ProtectedRoute>
-                <Troza />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/emplacado"
-            element={
-              <ProtectedRoute>
-                <Emplacado />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit-encolado"
-            element={
-              <ProtectedRoute>
-                <EditEncolado />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/procesos"
-            element={
-              <ProtectedRoute>
-                <ProcesosTable />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/multiple"
-            element={
-              <ProtectedRoute>
-                <Mult />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/users"
-            element={
-              <ProtectedRoute>
-                <UserTable />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/programa-produccion"
+          element={
+            <ProtectedRoute>
+              <Homeprocesos />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edicion"
+          element={
+            <ProtectedRoute>
+              <Edicion />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/encolado"
+          element={
+            <ProtectedRoute>
+              <Encol />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/troquelado"
+          element={
+            <ProtectedRoute>
+              <Troquel />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/trozado"
+          element={
+            <ProtectedRoute>
+              <Troza />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/emplacado"
+          element={
+            <ProtectedRoute>
+              <Emplacado />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-encolado"
+          element={
+            <ProtectedRoute>
+              <EditEncolado />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/procesos"
+          element={
+            <ProtectedRoute>
+              <ProcesosTable />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/multiple"
+          element={
+            <ProtectedRoute>
+              <Mult />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <ProtectedRoute>
+              <UserTable />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   </React.StrictMode>
 );
